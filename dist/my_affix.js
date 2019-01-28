b7a112abd17d924a5bb5e8ce7f92a8b050b69e64
caihong.teng.xu@gmail.com <caihong.teng.xu@gmail.com>
2019-01-28 16:27:50 +0800

    修改样式

---
+++ dist/my_affix.js
@@ -0,0 +1,13 @@
+$("body .header .header-inner").affix({
+    // 设置高度 0 为从置顶开始就浮动
+    offset: {
+        top: 0
+    }
+});
+
+$("body .main .main-inner .sidebar").affix({
+    offset: {
+        top: 0
+    }
+});
+
